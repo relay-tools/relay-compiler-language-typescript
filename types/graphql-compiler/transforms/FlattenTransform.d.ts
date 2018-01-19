f0837ee1b8e8f93c426cf0c1db9ecc29377d59fb
Eloy Durán <eloy.de.enige@gmail.com>
2018-01-19 15:57:41 +0100

    [types] Remove all unused typings.

--- types/graphql-compiler/transforms/FlattenTransform.d.ts
+++
@@ -1,9 +0,0 @@
-import { IRTransform } from '../core/GraphQLCompilerContext';
-
-export interface FlattenOptions {
-	flattenAbstractTypes?: boolean,
-	flattenInlineFragments?: boolean,
-}
-
-// Note this seems to be changing later on
-export function transformWithOptions(options: FlattenOptions): IRTransform;
--- types/relay-compiler/core/RelayFlowTypeTransformers.d.ts
+++
@@ -1,5 +0,0 @@
-export const transformInputType: Function;
-export const transformScalarType: Function;
-export type ScalarTypeMapping = {
-	[type: string]: string,
-};
