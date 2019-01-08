3d8e38e0e7d292fc1d967c8e7214aab82a6f4762
Tim Jacobi <timjacobi@fb.com>
2019-01-07 21:47:24 -0800

    support --noImplicitAny

---
+++ src/addAnyTypeCast.ts
@@ -0,0 +1,33 @@
+import * as ts from "typescript";
+
+export default function addAsAnyToObjectLiterals(oldSource: string): string {
+  function transformer<T extends ts.Node>(context: ts.TransformationContext) {
+    return function transform(rootNode: T) {
+      function visit(node: ts.Node): ts.Node {
+        if (node.kind === ts.SyntaxKind.ObjectLiteralExpression) {
+          return ts.createAsExpression(
+            node as ts.Expression,
+            ts.createTypeReferenceNode("any", [])
+          );
+        }
+        return ts.visitEachChild(node, visit, context);
+      }
+      return ts.visitNode(rootNode, visit);
+    };
+  }
+
+  const source = ts.createSourceFile(
+    "",
+    oldSource,
+    ts.ScriptTarget.ES2015,
+    true,
+    ts.ScriptKind.TS
+  );
+
+  const result = ts.transform(source, [transformer]);
+
+  const printer: ts.Printer = ts.createPrinter({
+    newLine: ts.NewLineKind.LineFeed
+  });
+  return printer.printFile(result.transformed[0] as ts.SourceFile);
+}
---
+++ src/loadCompilerOptions.ts
@@ -0,0 +1,20 @@
+import * as ts from "typescript";
+
+export const loadCompilerOptions = (): ts.CompilerOptions => {
+  const configFileName = ts.findConfigFile("", ts.sys.fileExists);
+  if (!configFileName) {
+    throw new Error("Could not find config file!");
+  }
+
+  const result = ts.readConfigFile(configFileName, ts.sys.readFile);
+  if (result.error) {
+    let message;
+    if (typeof result.error.messageText === "string") {
+      message = result.error.messageText;
+    } else {
+      message = result.error.messageText.messageText;
+    }
+    throw new Error(message);
+  }
+  return result.config.compilerOptions;
+};
