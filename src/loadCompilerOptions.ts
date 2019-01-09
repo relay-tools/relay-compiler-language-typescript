6233357011f047be31e5619510c48dce8c5514f6
Tim Jacobi <timjacobi@fb.com>
2019-01-08 21:17:31 -0800

    don't throw errors for missing or invalid config file

--- src/loadCompilerOptions.ts
+++ src/loadCompilerOptions.ts
@@ -3,18 +3,11 @@ import * as ts from "typescript";
 export const loadCompilerOptions = (): ts.CompilerOptions => {
   const configFileName = ts.findConfigFile(process.cwd(), ts.sys.fileExists);
   if (!configFileName) {
-    throw new Error("Could not find config file!");
+    return {};
   }
-
   const result = ts.readConfigFile(configFileName, ts.sys.readFile);
   if (result.error) {
-    let message;
-    if (typeof result.error.messageText === "string") {
-      message = result.error.messageText;
-    } else {
-      message = result.error.messageText.messageText;
-    }
-    throw new Error(message);
+    return {};
   }
   return result.config.compilerOptions;
 };
