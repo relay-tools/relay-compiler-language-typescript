0a6a0911b7ec8a54785ab8cd309985763b3eaa71
Justin Bennett <zephraph@gmail.com>
2019-10-21 11:29:22 -0400

    Pull before setting upstream

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,4 @@
 if [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  git config --local user.name "${GIT_NAME}"
-  git config --local user.email "${GIT_EMAIL}"
-  git remote rm origin
-  git remote add origin https://${GH_USER}:${GH_TOKEN}@github.com/relay-tools/relay-compiler-language-typescript.git
-  git branch --set-upstream-to origin/master
-
   npx auto shipit $AUTO_OPTS
 else
   echo "Not on master, skipping deploy"
