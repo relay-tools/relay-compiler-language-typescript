64de5146f47c1f25b49e4756697a805544a25589
thiagoleitedev <thiagoleitebr@gmail.com>
2020-07-23 14:21:59 -0300

    Call syncFixture from ts file

---
+++ syncFixture.ts
@@ -0,0 +1,46 @@
+import * as fs from "fs";
+import * as glob from "glob";
+import * as path from "path";
+
+type SyncFixturesType = {
+  souceFilePaths: Array<{
+    cwd: string;
+    pattern: string;
+  }>;
+  dest: string;
+};
+
+const syncFixtures = ({ souceFilePaths, dest }: SyncFixturesType) => {
+  souceFilePaths.forEach(({ cwd, pattern }) => {
+    glob
+      .sync(pattern, {
+        cwd
+      })
+      .forEach(file => {
+        try {
+          fs.copyFileSync(
+            path.join(__dirname, `${cwd}/${file}`),
+            `${dest}/${file}`
+          );
+          console.log(`${file} was copied`);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+  });
+};
+
+const souceFilePaths = [
+  {
+    cwd:
+      "../relay/packages/relay-compiler/language/javascript/__tests__/fixtures/flow-generator/useHaste",
+    pattern: "*.graphql"
+  },
+  {
+    cwd:
+      "../relay/packages/relay-compiler/codegen/__tests__/fixtures/compileRelayArtifacts",
+    pattern: "append-edge.graphql"
+  }
+];
+
+syncFixtures({ souceFilePaths, dest: "./teste" });
