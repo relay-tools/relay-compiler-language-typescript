21f6bf5ae619a5b557471d2af538c676023f77df
n1ru4l <laurinquast@googlemail.com>
2021-02-11 13:02:00 +0100

    wip: collect inline require statements and convert them to top level imports

--- src/formatGeneratedModule.ts
+++ src/formatGeneratedModule.ts
@@ -2,6 +2,36 @@ import { FormatModule } from "relay-compiler";
 import * as ts from "typescript";
 import addAnyTypeCast from "./addAnyTypeCast";
 
+const createRequireRegex = () => /require\("(.*)"\)/g;
+const escapeRegexString = (str: string) =>
+  str.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+
+// collects all require calls and converts them to top level imports
+const requireToImport = (content: string) => {
+  const requirePaths = new Set<string>();
+  const regex = createRequireRegex();
+  let result: null | RegExpExecArray = null;
+  while (true) {
+    result = regex.exec(content);
+    if (result === null) {
+      break;
+    }
+    requirePaths.add(result[1]);
+  }
+
+  for (const requirePath of requirePaths) {
+    const [baseName] = requirePath.replace("./", "").split(".");
+    content =
+      `import ${baseName} from "${requirePath.replace(".ts", "")}";\n` +
+      content.replace(
+        new RegExp(escapeRegexString(`require('${requirePath}')`), "g"),
+        baseName
+      );
+  }
+
+  return content;
+};
+
 export const formatterFactory = (
   compilerOptions: ts.CompilerOptions = {}
 ): FormatModule => ({
@@ -11,27 +41,31 @@ export const formatterFactory = (
   concreteText,
   typeText,
   hash,
-  sourceHash
+  sourceHash,
 }) => {
   const documentTypeImport = documentType
     ? `import { ${documentType} } from "relay-runtime";`
     : "";
   const docTextComment = docText ? "\n/*\n" + docText.trim() + "\n*/\n" : "";
-  let nodeStatement = `const node: ${documentType ||
-    "never"} = ${concreteText};`;
+  let nodeStatement = `const node: ${
+    documentType || "never"
+  } = ${concreteText};`;
   if (compilerOptions.noImplicitAny) {
     nodeStatement = addAnyTypeCast(nodeStatement).trim();
   }
-  return `/* tslint:disable */
-/* eslint-disable */
-// @ts-nocheck
-${hash ? `/* ${hash} */\n` : ""}
-${documentTypeImport}
-${typeText || ""}
+  const rawContent = `${typeText || ""}
 
 ${docTextComment}
 ${nodeStatement}
 (node as any).hash = '${sourceHash}';
 export default node;
 `;
+
+  const content = `/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+${hash ? `/* ${hash} */\n` : ""}
+${documentTypeImport}
+${requireToImport(rawContent)}`;
+  return content;
 };
